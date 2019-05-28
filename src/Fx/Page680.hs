da01860a6fbc8057145706c6f7f7c35471399762
Bjørn Arild Kristiansen <bjorn.arild.kristiansen@finn.no>
2019-05-28 17:30:17 +0200

    Add functors

--- src/Fx/Page680.hs
+++ src/Fx/Page680.hs
@@ -73,3 +73,61 @@ instance Functor (Flip K a) where
 testFlip = fmap (+1) (Flip (K 1)) == Flip (K 2)
 
 
+
+-- 4.
+data EvilGoateeConst a b = GoatyConst b
+
+instance Functor (EvilGoateeConst a) where
+  fmap f (GoatyConst b) = GoatyConst (f b)
+
+
+
+-- ★5★
+-- LiftItOut :: f a -> LiftItOut f a
+data LiftItOut f a = LiftItOut (f a) deriving (Eq, Show)
+
+instance Functor f => Functor (LiftItOut f) where
+  fmap g (LiftItOut fa) = LiftItOut (fmap g fa)
+
+testLiftItOut = fmap (+1) (LiftItOut (Just 1)) == LiftItOut (Just 2)
+
+
+-- ★6★
+-- DaWrappa :: f a -> g a -> Parappa f g a
+data Parappa f g a = DaWrappa (f a) (g a) deriving (Eq, Show)
+
+instance (Functor f, Functor g) => Functor (Parappa f g) where
+  fmap h (DaWrappa fa ga) = DaWrappa (fmap h fa) (fmap h ga)
+
+testDaWrappa = fmap (+1) (DaWrappa (Just 1) [2]) == DaWrappa (Just 2) [3]
+
+
+-- ★7★ --
+data IgnoreOne f g a b = IgnoringSomething (f a) (g b)
+
+instance Functor g => Functor (IgnoreOne f g a) where
+  fmap h (IgnoringSomething fa gb) = IgnoringSomething fa (fmap h gb)
+
+
+-- ★8★ --
+data Notorious g o a t = Notorious (g o) (g a) (g t)
+
+instance Functor g => Functor (Notorious g o a) where
+  fmap f (Notorious go ga gt) = Notorious (go) (ga) (fmap f gt)
+
+-- ★9★ --
+data List a =
+  Nil
+  | Cons a (List a)
+  deriving (Eq, Show)
+
+instance Functor List where
+  fmap f Nil = Nil
+  fmap f (Cons a (la)) = Cons (f a) (fmap f la)
+
+testList =
+  let
+    list = Cons 1 (Cons 2 (Cons 3 Nil))
+  in
+    fmap (*10) list == Cons 10 (Cons 20 (Cons 30 Nil))
+
